edd01408833db8163c907eb89749b91c80905861
Dimas F <dimaslimaferreira@gmail.com>
2024-01-31 10:36:17 -0300

    feat: add footer component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -9,11 +9,16 @@ import BarbershopItem from "./_components/barbershop-item";
 export default async function Home() {
   const barbershop = await db.barbershop.findMany({})
   return(
-  <div>
-    <Header />  
+    <div>
+    <Header />
+
     <div className="px-5 pt-5">
-      <h2 className="text-xl front-bold">Olá, Miguel</h2>
-      <p className="capitalize text-sm">{format(new Date(), "EEEE',' dd 'de' MMMM", {locale: ptBR})}</p>
+      <h2 className="text-xl font-bold">Olá, Miguel!</h2>
+      <p className="capitalize text-sm">
+        {format(new Date(), "EEEE',' dd 'de' MMMM", {
+          locale: ptBR,
+        })}
+      </p>
     </div>
 
     <div className="px-5 mt-6">
@@ -22,12 +27,23 @@ export default async function Home() {
 
     <div className="px-5 mt-6">
       <h2 className="text-xs mb-3 uppercase text-gray-400 font-bold">Agendamentos</h2>
-        <BookingItem />
+      <BookingItem />
     </div>
 
     <div className="mt-6">
-      <h2 className="psx-5 text-xs mb-3 uppercase text-gray-400 font-bold">Recomendandos</h2>
-      <div className="flex px-5 gap-4 overflow-y-auto [&::-webkit-scrollbar]:hidden">
+      <h2 className="px-5 text-xs mb-3 uppercase text-gray-400 font-bold">Recomendados</h2>
+
+      <div className="flex px-5 gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
+        {barbershop.map((barbershop) => (
+          <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+        ))}
+      </div>
+    </div>
+
+    <div className="mt-6 mb-[4.5rem]">
+      <h2 className="px-5 text-xs mb-3 uppercase text-gray-400 font-bold">Populares</h2>
+
+      <div className="flex px-5 gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
         {barbershop.map((barbershop) => (
           <BarbershopItem key={barbershop.id} barbershop={barbershop} />
         ))}
