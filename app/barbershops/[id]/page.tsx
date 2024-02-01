68df3366f9270f2d6c8108006d6d5c34e295d335
Dimas F <dimaslimaferreira@gmail.com>
2024-01-31 21:19:31 -0300

    feat: add barbershop info component

---
+++ app/barbershops/[id]/page.tsx
@@ -0,0 +1,31 @@
+import { db } from "@/app/_lib/prisma";
+import BarbershopInfo from "./_components/barbershop-info";
+
+
+interface BarbershopDetailsProps{
+    params: {
+        id?: string
+    };
+}
+
+const BarbershopDetailsPage = async ({params}: BarbershopDetailsProps) => {
+    if(!params.id){
+        // TODO redirecionar para home page
+        return null;
+    }
+
+    const barbershop = await db.barbershop.findUnique({
+    where: { 
+        id: params.id
+    },
+});
+
+    if(!barbershop){
+        // TODO redirecionar para home page
+        return null;
+    }
+
+    return <BarbershopInfo barbershop={barbershop} />
+};
+ 
+export default BarbershopDetailsPage;
