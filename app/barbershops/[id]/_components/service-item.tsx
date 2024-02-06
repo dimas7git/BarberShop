b4551cd26b9f88d2974de047ecd9872632e8ab73
Dimas F <dimaslimaferreira@gmail.com>
2024-02-06 10:51:45 -0300

    feat: ensure only available hours are shown on service booking

--- app/barbershops/[id]/_components/service-item.tsx
+++ app/barbershops/[id]/_components/service-item.tsx
@@ -9,11 +9,12 @@ import { signIn, useSession } from "next-auth/react";
 import Image from "next/image";
 import { useEffect, useMemo, useState } from "react";
 import { generateDayTimeList } from "../_helpers/hours";
-import { addDays, format, set, setHours, setMinutes } from "date-fns";
+import { addDays, format, set, setDay, setHours, setMinutes } from "date-fns";
 import { saveBooking } from "../_actions/save-booking";
 import { Loader2 } from "lucide-react";
 import { toast } from "sonner";
 import { useRouter } from "next/navigation";
+import { getDayBookings } from "../_actions/get-day-bookings";
 
 interface ServiceItemProps {
     barbershop: Barbershop;
@@ -28,7 +29,18 @@ const ServiceItem = ({ service, barbershop, isAuthenticated }: ServiceItemProps)
     const [hour, setHour] = useState<string | undefined>();
     const [submitIsLoading, setSubmitIsLoading] = useState(false);
     const [sheetIsOpen, setSheetIsOpen] = useState(false);
+    const [dayBookings, setDayBookings] = useState<Booking[]>([]);
 
+    useEffect(() => {
+        if(!date){
+            return
+        }
+        const refreshAvailableHours = async () => {
+            const _dayBookings = await getDayBookings(date);
+            setDayBookings(_dayBookings);
+        };
+        refreshAvailableHours();
+    }, [date])
     const handleDateClick = (date: Date | undefined) => {
         setDate(date);
         setHour(undefined);
@@ -80,8 +92,28 @@ const ServiceItem = ({ service, barbershop, isAuthenticated }: ServiceItemProps)
         }
     }
     const timeList = useMemo(() => {
-        return date ? generateDayTimeList(date) : [];
-    }, [date]);
+        if (!date) {
+          return [];
+        }
+    
+        return generateDayTimeList(date).filter((time) => {
+          const timeHour = Number(time.split(":")[0]);
+          const timeMinutes = Number(time.split(":")[1]);
+    
+          const booking = dayBookings.find((booking) => {
+            const bookingHour = booking.date.getHours();
+            const bookingMinutes = booking.date.getMinutes();
+    
+            return bookingHour === timeHour && bookingMinutes === timeMinutes;
+          });
+    
+          if (!booking) {
+            return true;
+          }
+    
+          return false;
+        });
+      }, [date, dayBookings]);
 
 
     return (
