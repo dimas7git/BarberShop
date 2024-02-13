b180cdade5ab4970a76c9e34802476cf334c3d83
Dimas F <dimaslimaferreira@gmail.com>
2024-02-12 22:06:05 -0300

    feat: ensure recommend bookings are show random

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -12,8 +12,13 @@ export default async function Home() {
   const session = await getServerSession(authOptions)
   const barbershop = await db.barbershop.findMany({});
 
-  const [barbershops, confirmedBookings] = await Promise.all([
+  const [barbershops, recommendBarbershops, confirmedBookings] = await Promise.all([
     db.barbershop.findMany({}),
+    db.barbershop.findMany({
+      orderBy: {
+        id: "asc",
+      }
+    }),
     session?.user
       ? db.booking.findMany({
         where: {
@@ -73,7 +78,7 @@ export default async function Home() {
         <h2 className="px-5 text-xs mb-3 uppercase text-gray-400 font-bold">Populares</h2>
 
         <div className="flex px-5 gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
-          {barbershop.map((barbershop) => (
+          {recommendBarbershops.map((barbershop) => (
             <div key={barbershop.id} className="min-w-[167px] max-w-[167px]">
               <BarbershopItem key={barbershop.id} barbershop={barbershop} />
             </div>
